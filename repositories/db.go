011e42eebf7e352d5e1f9c19318ff4a17569f3cc
zlfera <zlfera@gmail.com>
2017-12-24 21:49:00 +0800

    init

--- repositories/db.go
+++ repositories/db.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	db, err := gorm.Open("postgres", "user=u0_a124 dbname=youmile_dev")
+	db, err := gorm.Open("postgres", "sspzfyzexjagcv:32a3e10c18c74c343411ff1e1b8939e07ea73f168e9778506d6f21cffc0fb3d2@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/d1cina0osrksr5")
 	if err != nil {
 		return
 	}
